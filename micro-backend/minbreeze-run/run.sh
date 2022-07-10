22a5b7eb7c41267e0d46adfb6ae2e274735da19f
yumyai <yumyai@gmail.com>
2022-07-10 10:54:25 +0700

    Move pipeline into 16s folder

--- micro-backend/minbreeze-run/run.sh
+++
@@ -1,2 +0,0 @@
-nextflow ../minbreeze-flash/main.nf -c proj_spec.conf -profile conda,pbspro -w work_temp -resume \
-	-with-dag nflog/flowchart.png -with-timeline nflog/timeline.html -with-report nflog/report.html
